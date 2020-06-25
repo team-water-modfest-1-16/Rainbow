64f6aebd46a4de27700fa43ab5acb596fa1ebfb3
RypoFalem <rypofalem@gmail.com>
2020-06-25 11:26:06 -0500

    use correct item class

--- src/main/java/hpru/RainbowMod.java
+++ src/main/java/hpru/RainbowMod.java
@@ -1,16 +1,15 @@
 package hpru;
 
+import hpru.item.RainbowItem;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class RainbowMod implements ModInitializer {
 
-    public static final Item RAINBOW_ITEM = new Item(
+    public static final RainbowItem RAINBOW_ITEM = new RainbowItem(
             new Item.Settings()
                     .group(ItemGroup.COMBAT)
                     .maxDamage(500)
