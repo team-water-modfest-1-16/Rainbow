21af7f5d605eb0314fb1963c31f505803ff6b4b4
RypoFalem <rypofalem@gmail.com>
2020-06-25 11:09:33 -0500

    create basic item

---
+++ src/main/java/hpru/item/RainbowItem.java
@@ -0,0 +1,9 @@
+package hpru.item;
+
+import net.minecraft.item.BowItem;
+
+public class RainbowItem extends BowItem {
+    public RainbowItem(Settings settings) {
+        super(settings);
+    }
+}
